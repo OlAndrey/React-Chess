b8624c082b4264f222dfa6749ac7b3b1d645c073
OlAndrey <oleynik.andrey01@gmail.com>
2023-10-19 16:15:26 +0300

    feat(server): added handling of players joining a room

--- server/index.js
+++ server/index.js
@@ -53,4 +53,44 @@ io.sockets.on('connection', (socket) => {
     if (!game) socket.emit('token-invalid')
     else socket.emit('token-ok')
   })
+
+  socket.on('join', (data) => {
+    const game = _games.get(data.token)
+
+    if (!game) {
+      socket.emit('token-invalid')
+      return
+    }
+
+    const nOfPlayers = game.get('players').size
+    const colors = ['black', 'white']
+    let color
+
+    clearTimeout(game.get('timeout'))
+
+    if (nOfPlayers >= 2) {
+      socket.emit('room-full')
+      return
+    } else if (nOfPlayers === 1) {
+      if (game.getIn(['players', 0, 'color']) == 'black') color = 'white'
+      else color = 'black'
+    } else {
+      color = colors[Math.floor(Math.random() * 2)]
+    }
+
+    // join room
+    socket.join(data.token)
+
+    _games = _games.updateIn([data.token, 'players'], (players) =>
+      players.push(
+        Map({
+          socket: socket,
+          color: color,
+          time: game.get('gameTime')
+        })
+      )
+    )
+
+    socket.emit('joined', { color: color, time: game.get('gameTime') })
+  })
 })
