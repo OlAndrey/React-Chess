39bbec5feb0f808764af35c94f3585989050ab67
OlAndrey <oleynik.andrey01@gmail.com>
2023-10-19 20:28:37 +0300

    chore(client): added processing of connecting to an online game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { MutableRefObject, useRef, useState } from 'react'
 import Modal from './Components/Modal'
 import socket from './helpers/socket'
 
-function App() {
+const App = () => {
   const [showModal] = useState(true)
   const [isNewGame, setIsNewGame] = useState(false)
+  const inputRef = useRef() as MutableRefObject<HTMLInputElement>
+
   const data = [
     { time: '5', name: 'Blitz' },
     { time: '10', name: 'Rapid' },
@@ -12,17 +14,33 @@ function App() {
     { time: '-', name: 'Unlimit' }
   ]
 
+  const connect = () => {
+    if (inputRef.current.value.trim()) {
+      socket.emit('check-token', { token: inputRef.current.value })
+      socket.on('full', () => console.error('room is full'))
+      socket.on('token-invalid', () => console.error('invalid'))
+      socket.on('token-ok', () => console.log('token is ok'))
+    }
+  }
+
   const createGame = (option: (typeof data)[0]) => {
-    socket.emit('start', { time: option.time })
+    socket.emit('create-game', { time: option.time })
     console.log(option)
   }
 
   return (
     <Modal className={showModal ? 'bg-image' : ''} isShow={showModal}>
       {!isNewGame ? (
-        <button className="btn" onClick={() => setIsNewGame(true)}>
-          Create
-        </button>
+        <>
+          <input type="text" ref={inputRef} placeholder="Enter game id" />
+          <button className="btn bg-black" onClick={connect}>
+            Connect
+          </button>
+          <div className="text-center">or</div>
+          <button className="btn bg-green" onClick={() => setIsNewGame(true)}>
+            Create
+          </button>
+        </>
       ) : (
         <>
           <div className="flex">
